02c8946efd5cc58603caad3a6c1b4fbee9f2dbcd
panjiawen906 <294190056@qq.com>
2019-07-29 22:00:25 +0800

    bug

---
+++ src/main/java/com/main/register/controller/IndexController.java
@@ -0,0 +1,33 @@
+package com.main.register.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping(value = "/api")
+public class IndexController {
+
+    @RequestMapping(value = "/index")
+    public String index(HttpServletRequest request) {
+        String url = "";
+        String requestHeader = request.getHeader("user-agent");
+        String[] deviceArray = new String[]{"android", "iphone","ipad","windows phone"};
+        //判断是否pc端登陆
+        boolean pc = false;
+        if (requestHeader == null) {
+            pc = true;
+        }
+        requestHeader = requestHeader.toLowerCase();
+        for (String device : deviceArray) {
+            if (requestHeader.indexOf(device) != -1) {
+                pc = false;
+                break;
+            }
+        }
+        if(pc) url = "/pc_index.html";
+        else url = "/app_index.html";
+        return url;
+    }
+}
